57a8150d3906aaccf672ae33a7194e516b8f8299
Lucas Jansen <wlaj@Macbook.local>
2022-08-19 11:05:52 +0200

    Added preview

--- pages/index.js
+++ pages/index.js
@@ -1,47 +1,51 @@
-import Head from "next/head"
-import styles from "../styles/Home.module.css"
-
-import { getStoryblokApi, StoryblokComponent, useStoryblokState } from "@storyblok/react"
-
-export default function Home({ story }) {
-  story = useStoryblokState(story);
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+ 
+import {
+  useStoryblokState,
+  getStoryblokApi,
+  StoryblokComponent,
+} from "@storyblok/react";
+ 
+export default function Home({ story, preview }) {
+  story = useStoryblokState(story, {}, preview);
+ 
   return (
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+ 
       <header>
-        <h1>
-          {story ? story.name : 'My Site'}
-        </h1>
+        <h1>{story ? story.name : "My Site"}</h1>
       </header>
-
-      <main>
-        <StoryblokComponent blok={story.content} />
-      </main>
+ 
+      <StoryblokComponent blok={story.content} />
     </div>
-  )
+  );
 }
-
-export async function getStaticProps() {
-  // home is the default slug for the homepage in Storyblok
+ 
+export async function getStaticProps(context) {
   let slug = "home";
-
-  // load the draft version
+ 
   let sbParams = {
-    version: "draft", // or 'published'
+    version: "published",
   };
-
+ 
+  if (context.preview) {
+    sbParams.version = "draft";
+  }
+ 
   const storyblokApi = getStoryblokApi();
   let { data } = await storyblokApi.get(`cdn/stories/${slug}`, sbParams);
-
+ 
   return {
     props: {
       story: data ? data.story : false,
       key: data ? data.story.id : false,
+      preview: context.preview || false,
     },
-    revalidate: 3600, // revalidate every hour
+    revalidate: 3600,
   };
 }
