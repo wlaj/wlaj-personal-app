1ce8fee3a35829445ac2bd6223085cadc3757c12
Lucas Jansen <wlaj@Macbook.local>
2022-08-31 09:22:48 +0200

    Changed blog

--- pages/_app.js
+++ pages/_app.js
@@ -1,38 +1,33 @@
-import { storyblokInit, apiPlugin } from "@storyblok/react";
-import Feature from "../components/Feature";
-import Grid from "../components/Grid";
-import Page from "../components/Page";
-import Teaser from "../components/Teaser";
-import Config from "../components/Config";
-import HeaderMenu from "../components/HeaderMenu";
-import MenuLink from "../components/MenuLink";
+import '../styles/tailwind.css';
+import { storyblokInit, apiPlugin } from '@storyblok/react';
+import Feature from '../components/Feature';
+import Grid from '../components/Grid';
+import Page from '../components/Page';
+import Teaser from '../components/Teaser';
 import Hero from '../components/Hero';
 import AllArticles from '../components/AllArticles';
 import Article from '../components/Article';
 import PopularArticles from '../components/PopularArticles';
- 
+
 const components = {
   feature: Feature,
   grid: Grid,
   teaser: Teaser,
   page: Page,
-  config: Config,
   hero: Hero,
-  article: Article,
   "all-articles": AllArticles,
+  article: Article,
   "popular-articles": PopularArticles,
-  "header_menu": HeaderMenu,
-  "menu_link": MenuLink
 };
- 
+
 storyblokInit({
   accessToken: process.env.PREVIEW_TOKEN,
   use: [apiPlugin],
-  components
+  components,
 });
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,57 +1,46 @@
-import Head from "next/head";
- 
+import Head from 'next/head';
+
 import {
   useStoryblokState,
   getStoryblokApi,
   StoryblokComponent,
-} from "@storyblok/react";
-import Layout from "../components/Layout";
- 
+} from '@storyblok/react';
+import Layout from '../components/Layout';
+
 export default function Home({ story }) {
   story = useStoryblokState(story, {
-    resolveRelations: ["popular-articles.articles"],
-});
- 
+    resolveRelations: ['popular-articles.articles'],
+  });
+
   return (
     <div>
       <Head>
-        <title>Digics | Web and marketing agency</title>
+        <title>Home</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {/* <header>
-        <h1>{story ? story.name : "My Site"}</h1>
-      </header> */}
       <Layout>
-      <StoryblokComponent blok={story.content} />
+        <StoryblokComponent blok={story.content} />
       </Layout>
     </div>
   );
 }
- 
-export async function getServerSideProps(context) {
-  // get the query object
-  const insideStoryblok = context.query._storyblok;
-  const shouldLoadDraft = context.preview || insideStoryblok;
- 
-  let slug = "home";
- 
+
+export async function getStaticProps() {
+  let slug = 'home';
+
   let sbParams = {
-    version: "draft", // or 'published',
-    resolve_relations: ["popular-articles.articles"],
+    version: 'draft', // or 'published'
+    resolve_relations: ['popular-articles.articles'],
   };
- 
-  if (shouldLoadDraft) {
-    sbParams.version = "draft";
-  }
- 
+
   const storyblokApi = getStoryblokApi();
   let { data } = await storyblokApi.get(`cdn/stories/${slug}`, sbParams);
- 
+
   return {
     props: {
       story: data ? data.story : false,
       key: data ? data.story.id : false,
-      preview: shouldLoadDraft || false,
     },
+    revalidate: 3600,
   };
 }
