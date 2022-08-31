5e3bb2ad29324fd32d2a035d3c3bdffc06452d4f
Lucas Jansen <wlaj@Macbook.local>
2022-08-31 08:48:22 +0200

    Added hero module

--- pages/_app.js
+++ pages/_app.js
@@ -3,12 +3,20 @@ import Feature from "../components/Feature";
 import Grid from "../components/Grid";
 import Page from "../components/Page";
 import Teaser from "../components/Teaser";
+import Config from "../components/Config";
+import HeaderMenu from "../components/HeaderMenu";
+import MenuLink from "../components/MenuLink";
+import Hero from '../components/Hero';
  
 const components = {
   feature: Feature,
   grid: Grid,
   teaser: Teaser,
   page: Page,
+  config: Config,
+  hero: Hero,
+  "header_menu": HeaderMenu,
+  "menu_link": MenuLink
 };
  
 storyblokInit({
